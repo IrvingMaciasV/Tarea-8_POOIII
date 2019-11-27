e012c79a7cffa8d33f549d15105c574cbba935bf
IrvingMaciasV <irving_macias@hotmail.com>
2019-11-26 19:21:52 -0600

    1p

---
+++ Tarea 8_POOIII/Assets/CubeSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.instance;
+    }
+    private void FixedUpdate()
+    {
+        ObjectPooler.instance.SpawnFromPool("Cube", transform.position, Quaternion.identity);
+    }
+}
